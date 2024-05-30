9a8c3b77c6887a9f43cc9d9a5bc60f797ae1bb57
Yizheng Huang <huangyz0918@gmail.com>
2024-05-30 16:40:28 -0700

    ✨ feat: removed the api test

--- tests/test_api.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-if __name__ == "__main__":
-    with requests.get('http://localhost:8000/chat', params={'project': 'test', 'message': 'hello?'}, stream=True) as r:
-        for chunk in r.iter_content():
-            print(chunk)
