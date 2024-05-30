0a5c02a88ef7b9e6979099b844bbd27612bf37fb
Yizheng Huang <huangyz0918@gmail.com>
2024-05-29 17:57:55 -0700

    🔨 refactor: removed the poc

--- poc/imdb_project/model_selection_GENERATED.py
+++
@@ -1,4 +0,0 @@
-from transformers import DistilBertForSequenceClassification, DistilBertTokenizer
-
-model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
-tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
