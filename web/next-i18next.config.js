66c38eec70d0e6c48d0649b43f742ae89ea53cd2
Yizheng Huang <huangyz0918@gmail.com>
2024-05-30 17:56:40 -0700

    ✨ feat: re-init the chatui

---
+++ web/next-i18next.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  i18n: {
+    defaultLocale: "en",
+    locales: [
+      "bn",
+      "de",
+      "en",
+      "es",
+      "fr",
+      "he",
+      "id",
+      "ja",
+      "ko",
+      "pt",
+      "ru",
+      "sv",
+      "te",
+      "vi",
+      "zh",
+    ],
+  },
+  localePath:
+    typeof window === 'undefined'
+      ? require('path').resolve('./public/locales')
+      : '/public/locales',
+};
