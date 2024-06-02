8ce280692ef01178ddc16e361e8ac73c8657a033
huaizheng hunter zhang <huaizheng@breezeml.ai>
2024-06-01 17:37:30 -0700

    ask users to select debug env

--- agent/function/tech_leader.py
+++ agent/function/tech_leader.py
@@ -190,15 +190,23 @@ class LeaderAgent:
                 code_generation_agent.invoke(task_num, self.requirement)
 
                 # install the dependencies for this plan and code.
+                self.console.log("[bold red]Step 6: Setup dependencies [bold red]")
+                self.project.debug_env = questionary.select(
+                    "Select the debug environment:",
+                    choices=['just_generate_code', 'local', 'cloud']
+                ).ask()
+                update_project_state(self.project)
                 existing_code = read_file_to_string(self.project.entry_file)
                 setup_agent = SetupAgent(self.agent)
                 setup_agent.invoke(existing_code)
 
                 # code reflection
                 if self.project.debug_env != 'just_generate_code':
-                    self.console.log("[bold red]Step 6: Code execution and reflection[bold red]")
+                    self.console.log("[bold red]Step 7: Code execution and reflection[bold red]")
                     code_reflection_agent = ReflectAgent(self.agent, self.project)
                     code_reflection_agent.invoke(self.requirement)
+                else:
+                    self.console.log("The code execution and reflection are skipped.")
 
                 is_running = False
         except KeyboardInterrupt:
