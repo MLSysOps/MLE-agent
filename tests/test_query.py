70331965c719b49620c9e256efdf21134b2d39e0
Yizheng Huang <huangyz0918@gmail.com>
2024-07-03 12:50:23 -0700

    🚑 quickfix: removed test files

--- tests/test_query.py
+++
@@ -1,7 +0,0 @@
-from mle.agents import CodeAgent
-from mle.model import load_model
-
-if __name__ == '__main__':
-    model = CodeAgent(load_model('/Users/huangyz0918/desktop/test', 'gpt-4o'))
-    print(model.handle_query(
-        "Search the most popular machine learning algorithm, and implement it in Python project."))
