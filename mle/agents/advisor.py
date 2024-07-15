9481ba9fda482005f6e6d2e5583e1ad6348cc597
Yizheng Huang <huangyz0918@gmail.com>
2024-07-15 12:33:30 -0700

    ✨ feat: improved the advisor

--- mle/agents/advisor.py
+++ mle/agents/advisor.py
@@ -1,7 +1,23 @@
+import sys
 import json
 
 from mle.function import *
 from mle.utils import get_config
+from mle.utils import print_in_box
+
+
+def process_report(requirement: str, suggestions: dict):
+    return textwrap.dedent(f"""
+                {requirement}
+    The ML task: {suggestions.get('task')}
+    The model or algorithm: {suggestions.get('model_or_algorithm')},
+    The training method: {suggestions.get('training_method')},
+    The serving method: {suggestions.get('serving_method')},
+    The reference: {suggestions.get('reference')},
+    The evaluation metric: {suggestions.get('evaluation_metric')},
+    The device: {suggestions.get('device')},
+    The suggestion: {suggestions.get('suggestion')}
+    """).strip()
 
 
 class AdviseAgent:
@@ -16,23 +32,25 @@ class AdviseAgent:
             model: the model to use.
         """
         config_data = get_config()
+        self.report = None
         self.model = model
         self.chat_history = []
         self.ask_history = []
         self.sys_prompt = """
-        You are an Machine learning expert tasked with advising on the best machine learning task/model/dataset to use.
+        You are an Machine learning expert tasked with advising on the best ML task/model/algorithm to use.
         
         Your capabilities include:
 
-        1. Read and understand the user's requirements, the requirements may include the task, the dataset, the model,
-            and the evaluation metrics. You should always follow the user's requirements, for example, if the user
-            has provided a local dataset, you should use the path of the dataset.
-        2. Read the Q&A history to understand the user's intent and the context of the conversation.
-        3. And then you should use the function `search_arxiv` to search the state-of-the-art machine learning
-         tasks/models/datasets that can be used to solve the user's requirements, and stay up-to-date with the latest.
-        4. If the user does not provide the details (task/model/dataset/metric), you should always suggest them.
-        5. You should provide the paper reference of the task/model/dataset/metric you suggest. You use the search
-         results from the function `search_arxiv` or `search_papers_with_code` by generating keywords.
+        1. Read and understand the dataset information and user's requirements, the requirements may include the task,
+         the model, and the evaluation metrics, etc. You should always follow the user's requirements.
+        2. And then you should use the function `search_arxiv` or `search_papers_with_code` to search the
+         state-of-the-art machine learning tasks/models that can be used to solve the user's requirements, and stay
+         up-to-date with the latest.
+        3. If the user does not provide the details (task/model/dataset/metric), you should always suggest them.
+        4. You should provide the paper reference of the task/model/algorithm/metric you suggest. You use the search
+         results from the function `search_arxiv` or `search_papers_with_code` by generated search keywords.
+        5. The suggestion should be as detailed as possible, include the SOTA methods for data processing, feature
+         extraction, model selection, training/sering methods and evaluation metrics. And the reasons why you suggest.
         """
         self.ask_prompt = """
         You are an Machine learning expert, now you are talking to the user to help them write better ML project
@@ -60,35 +78,39 @@ class AdviseAgent:
         """
         self.search_prompt = """
         6. You should also use function `web_search` to search for articles, papers, or tutorials related to the
-         task/model/dataset/metric to help you decide which one to use.
+         task/model/algorithm/metric to help you decide which one to use.
         """
         self.json_mode_prompt = """
 
         Example JSON output:
         
         {
-              "task":"image-classification",
-              "model":"Random Forest",
-              "dataset":"iris",
-              "reference": ["10.1109/ICDM50108.2020.00131"],
-              "evaluation_metric": ["accuracy", "precision", "recall", "f1"],
-              "suggestion:" "Based on the user requirement, I suggest the user to use the Random Forest model, which is
-               suitable for the iris dataset, you can use the accuracy, precision, recall, and f1 as the evaluation
-                metrics."
+            "task":"text-classification",
+            "model_or_algorithm":"BERT-base",
+            "reference": ["https://arxiv.org/abs/1810.04805"],
+            "evaluation_metric": ["accuracy", "f1"],
+            "training_method": "fine-tuning",
+            "serving_method": "Serving is not required",
+            "device": "Local CPU",
+            "suggestion": "Based on the user requirement, we suggest you to fine-tune a pre-trained BERT-base model for
+            your text classification dataset. You can use the accuracy and f1 as the evaluation metrics."
         }
         
         {
-            "task":"text-classification",
-            "model":"BERT-base",
-            "dataset":"/home/user/dataset.csv",
-            "reference": ["https://arxiv.org/abs/1810.04805"],
-            "evaluation_metric": ["accuracy", "f1"],
-            "suggestion": "Based on the user requirement, BERT is suitable for extracting the features from the dataset
-             you provided, you can use the accuracy and f1 as the evaluation metrics."
+            "task":"image-feature-extraction",
+            "model_or_algorithm":"Pre-trained ResNet-50",
+            "reference": ["https://arxiv.org/abs/2310.02037"],
+            "evaluation_metric": ["accuracy"],
+            "training_method": "Training is not required",
+            "serving_method": "Serving is not required",
+            "device": "GPU",
+            "suggestion": "Based on the user's data and the requirement, we recommend using the pre-trained ResNet-50
+             model to extract the features from the images, and use the accuracy as the evaluation metric. GPU is
+              required for the feature extraction."
         }
         
         """
-        self.functions = [schema_search_arxiv, schema_search_papers_with_code]
+        self.functions = [schema_web_search, schema_search_arxiv, schema_search_papers_with_code]
         if config_data.get('search_key'):
             self.functions.append(schema_web_search)
             self.sys_prompt += self.search_prompt
@@ -142,4 +164,35 @@ class AdviseAgent:
         )
 
         self.chat_history.append({"role": "assistant", "content": text})
-        return json.loads(text)
+        suggestions = json.loads(text)
+        return process_report(requirement, suggestions)
+
+    def interact(self, requirement):
+        """
+        Interact with the user to ask and suggest.
+        Args:
+            requirement: the initial user requirements.
+        """
+        self.report = self.suggest(requirement)
+        print_in_box(self.report, title="MLE Advisor", color="green")
+        while True:
+            question = questionary.text(
+                "Suggestions to improve the report? (empty answer or \"no\" to move to the next stage)").ask()
+
+            if not question or question.lower() in ["no"]:
+                break
+
+            if question.lower() in ["exit"]:
+                sys.exit(0)
+
+            self.chat_history.append({"role": "user", "content": question})
+            suggestions = self.model.query(
+                self.chat_history,
+                function_call='auto',
+                functions=self.functions,
+                response_format={"type": "json_object"}
+            )
+
+            self.report = process_report(requirement, json.loads(suggestions))
+            print_in_box(self.report, title="MLE Advisor", color="green")
+        return self.report
