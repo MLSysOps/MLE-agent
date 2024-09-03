db360825da45ec4b385c47c4c84a9033479073a6
Yizheng Huang <huangyz0918@gmail.com>
2024-09-02 19:38:45 -0700

    🚑 quickfix:

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,7 +1,7 @@
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx'
-});
+    themeConfig: './theme.config.tsx',
+})
 
 module.exports = withNextra({
     output: 'export',
