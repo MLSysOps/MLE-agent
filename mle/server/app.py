9f8c2168a057c020531977e70f7dff969501c53f
Yizheng Huang <huangyz0918@gmail.com>
2024-09-05 10:14:34 -0700

    ✨ feat: build the `server` and the corresponding cli entry

---
+++ mle/server/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    """
+    read_root: read the root.
+    :return: the root.
+    """
+    return {"Welcome to": "MLE-Agent!"}
